40ebba598d5c127cd3b12d09185878b6866c865e
Wanderson <bsilva.wanderson@gmail.com>
2019-11-16 03:07:59 -0300

    read all .txt files in input dir

--- main/main.py
+++ main/main.py
@@ -3,63 +3,51 @@ from lexical_analyzer.token.token import Token
 from lexical_analyzer.util.clean_code import CleanCode
 
 import os
-import sys
 import glob
 
 tableSimbols = []   # Tabela de Simbolos
 filesText = []
-cleanSourceCode = []
+lexInputCodes = []
 auxLexemes = []
 
 
 def main():
-    code = read_file("../input/eu.txt");
+    codes = read_file("../input/");
     cc = CleanCode()
     is_block_comment = False
     newLine = ""
-    for line in code:
-        is_block_comment, newLine = cc.remove_comments(is_block_comment, line);
-        cleanSourceCode.append(newLine)
+    for code in codes:
+        cleanSourceCode = []
+        for line in code:
+            is_block_comment, newLine = cc.remove_comments(is_block_comment, line);
+            cleanSourceCode.append(newLine)
+        lexInputCodes.append(cleanSourceCode)
     if(is_block_comment):
         print("comentário mal formado") #arrumar isso aqui depois. Tem que gerar erro
     
-    tableSimbols = lex_analyser(cleanSourceCode)
+    tableSimbols = lex_analyser(lexInputCodes[0])
     print(tableSimbols)
         
 
 
 def lex_analyser(cleanSourceCode):
+    pass
     la = LexicalAnalyzer(cleanSourceCode)
     classe = la.identifyToken()
     return classe
     
 
 def read_file(path):
-    with open(path) as f:
-        lines = f.read().splitlines()      
-    return lines
-
-def readFilesAndUpdateFilesTextAux():
-    numberFiles = len(glob.glob('../input/*.txt'))
-
+    numberFiles = len(glob.glob(path+'*.txt'))
     if (numberFiles == 0):
         print ("A pasta de entrada está vazia!")
-
-    else:
-        i = 0
-        for i in range(0, numberFiles):
-            auxFile = os.path.isfile('../input/entrada' + str(i) + '.txt')  # Se o arquivo existir na pasta, prossegue
-            if auxFile:
-                fileText = []
-                fileAux = open('../input/entrada' + str(i) + '.txt', "r")  # Abre o arquivo de texto específico
-                lineText = fileAux.readline()  # Tenta ler uma linha do arquivo de texto
-                while lineText is not None:  # Enquanto encontrar uma linha no arquivo de texto, consome o mesmo e
-                    # coloca na lista de linhas da lista de arquivo de textos
-                    fileText.append(lineText)
-                    lineText = fileAux.readline()
-                filesText.append(fileText)  # Adiciona a lista de linhas na lista de arquivos de texto
-        if len(filesText) == 0:
-            print ("Não foi encontrado nenhum arquivo de texto com o formato necessário (entradaX.txt)")
+    codes = []
+    for i in range (numberFiles):
+        file = path+"entrada"+str(i)+'.txt'
+        with open(file) as f:
+            codes.append(f.read().splitlines())
+            
+    return codes
 
 
 if __name__ == "__main__":
