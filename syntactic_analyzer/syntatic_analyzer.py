ceba89e13c71a8b0fd9a56310389f548107f8dae
Aloisio Junior <kleyner2@hotmail.com>
2020-02-18 01:05:31 -0300

    Update syntatic_analyzer.py

--- syntactic_analyzer/syntatic_analyzer.py
+++ syntactic_analyzer/syntatic_analyzer.py
@@ -8,4 +8,50 @@ import re
 
 class SyntaticAnalyzer:
     
-    def __init__(self):
+    def __init__(self, listTokens):
+        self.currentToken = 0
+        self.previousToken = 0
+        self.numberFile = 0
+        self.lexemToken = ""
+        self.errorLineToken = 0
+        self.listTokens = listTokens
+        self.FirstGlobalValues = []
+        self.FirstType = []
+        self.FirstFunctions_Procedures = []
+        self.FirstConstValuesDeclaration = []
+        self.FirstConstValuesAttribution = []
+        self.FirstConstMoreAttributions = []
+        self.FirstVarValuesDeclaration = []
+        self.FirstFunctions_Procedures = []
+        self.FirstValueConst = []
+        self.FirstNumber = []
+        self.FirstBooleanos = []
+        self
+        
+        FirstGlobalValues.append("const")
+        FirstGlobalValues.append("var")
+        FirstType.append("int")
+        FirstType.append("real")
+        FirstType.append("boolean")
+        FirstType.append("string")
+        FirstConstValuesDeclaration.append(FirstType)
+        FirstConstMoreAttributions.append(",")
+        FirstConstValuesAttribution.append("IDE")
+        FirstBooleanos.append("true")
+        FirstBooleanos.append("false")
+        FirstValueConst.append(FirstNumber)
+        FirstValueConst.append("CDC")
+        FirstValueConst.append(FirstBooleanos)
+        
+    
+    def starSyntaticAnalyzer(self):
+        self.getNextToken()
+        
+        
+    def getNextToken(self):
+        token = self.listTokens[self.currentToken]
+        self.lexemToken = token.lexema
+        self.errorLineToken = token.linha
+        self.previousToken = self.currentToken
+        self.currentToken = self.currentToken + 1
+        
