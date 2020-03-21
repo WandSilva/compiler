2829b8259193b0e2cc6fc3e728ac6ec75679b3cb
wandsilva <bsilva.wanderson@gmail.com>
2020-03-21 17:14:18 -0300

    melhorias nas tabelas de simbolos

--- semantic_analyzer/semantic.py
+++ semantic_analyzer/semantic.py
@@ -2,34 +2,40 @@ class semantic_analyzer:
     
     def __init__(self):
         self.table_var = dict()
-        self.table_struct = dict(ide = [], type_atrributes = [], atrributes = [], scope = [], extend = [])
+        self.table_struct = dict()
         self.table_array = dict()
         self.table_func = dict(ide = [], tipo = [], type_params = [], params = [], num_params = [])
 
     #MÉTODOS PARA MANIPULAR A TABELA DE VARIÁVEIS
     def contains_var(self, scope, ide):
         return True if ide in self.table_var[scope]['ide'] else False
- 
 
     def add_var(self, scope, tipo, ide, value):
+        if scope not in self.table_var:
+            self.__add_var_scope(scope)
         self.table_var[scope]['tipo'].append(tipo)
         self.table_var[scope]['ide'].append(ide)
         self.table_var[scope]['value'].append(value)
 
-    def add_var_scope(self, scope):
+    def __add_var_scope(self, scope):
         self.table_var[scope] = dict(tipo = [], ide = [], value = [])
 
 
     #MÉTODOS PARA MANIPULAR A TABELA DE STRUCTS
-    def contains_struct(self, ide):
-        return True if ide in self.table_struct['ide'] else False
+    def contains_struct(self, ide, scope):
+        return True if ide in self.table_struct[scope]['ide'] else False
 
-    def add_struct(self, ide, type_atrributes, atrributes, scope, extend):
-        self.table_struct['ide'].append(ide)
-        self.table_struct['type_atrributes'].append(type_atrributes)
-        self.table_struct['atrributes'].append(atrributes)
-        self.table_struct['scope'].append(scope)
-        self.table_struct['extend'].append(extend)
+    def add_struct(self, scope, ide, type_atrributes, atrributes, extend):
+        if scope not in self.table_struct:
+            self.__add_struct_scope(scope)
+        dict(ide = [], type_atrributes = [], atrributes = [], extend = [])
+        self.table_struct[scope]['ide'].append(ide)
+        self.table_struct[scope]['type_atrributes'].append(type_atrributes)
+        self.table_struct[scope]['atrributes'].append(atrributes)
+        self.table_struct[scope]['extend'].append(extend)
+
+    def __add_struct_scope(self, scope):
+        self.table_struct[scope] = dict(ide = [], type_atrributes = [], atrributes = [], extend = [])
 
 
     
