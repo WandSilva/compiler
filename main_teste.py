7d235b56ea2598963971227697979ed338fdc57c
wandsilva <bsilva.wanderson@gmail.com>
2020-03-29 04:17:55 -0300

    operacoes com as tabelas de simbolos

---
+++ main_teste.py
@@ -0,0 +1,28 @@
+#   EXA869 - MI - Processadores de Linguagem de Programação (2019.2)
+#   Discentes: Aloisio Junior e Wanderson Silva
+
+#   ARQUIVO PRINCIPAL PARA CHAMADA DOS ANALISADORES
+
+from semantic_analyzer.semantic import semantic_analyzer
+
+semantic = semantic_analyzer()
+
+#teste add primitivo
+#semantic.add_var('start', 'int', 'x', None)
+#semantic.add_var('start', 'string', 'y', 'cvfr333.5')
+#semantic.assign_var('start', 'x', '1', 'primitivo')
+#semantic.assign_var('start', 'y', 'novo', 'primitivo')
+
+#teste add variavel
+#semantic.add_var('start', 'int', 'x', None)
+#semantic.add_var('start', 'float', 'y', '5.1')
+#semantic.assign_var('start', 'x', 'y', 'variavel')
+
+
+#teste add retorno funcao
+semantic.add_var('start', 'int', 'x', None)
+semantic.add_func('add', 'float', ['int', 'int'], ['a', 'b'])
+semantic.assign_var('start', 'x', 'add', 'func')
+
+
+print(semantic.get_vars())
--- semantic_analyzer/semantic.py
+++ semantic_analyzer/semantic.py
@@ -1,3 +1,5 @@
+import re 
+
 class semantic_analyzer:
     
     def __init__(self):
@@ -8,28 +10,107 @@ class semantic_analyzer:
         self.semantic_errors = []
 
     #MÉTODOS PARA MANIPULAR A TABELA DE VARIÁVEIS
-    def contains_var(self, scope, ide):
+    def __contains_var(self, scope, ide):
         if scope in self.table_var.keys():
             if ide in self.table_var[scope]['ide']:
                 return True
         return False
 
     def add_var(self, scope, tipo, ide, value):
-        if scope not in self.table_var.keys():
-            self.__add_var_scope(scope)
-            print("HHHHHHHHHHHHHHHHHHHHHHHHH")
+        if self.__contains_var(scope, ide):
+            print('FAZER A CHAMADA DO ERRO AQUI: #DECLARANDO UMA JA EXISTENTE') #DECLARANDO UMA JA EXISTENTE
+        else:
+            if scope not in self.table_var.keys():
+                self.__add_var_scope(scope)
+            self.table_var[scope]['tipo'].append(tipo)
+            self.table_var[scope]['ide'].append(ide)
+            self.table_var[scope]['value'].append(value)
 
-        self.table_var[scope]['tipo'].append(tipo)
-        self.table_var[scope]['ide'].append(ide)
-        self.table_var[scope]['value'].append(value)
 
     def __add_var_scope(self, scope):
         self.table_var[scope] = dict(tipo = [], ide = [], value = [])
 
-    def assign_var(self, scope, ide, value):
-        index = self.table_var[scope]['ide'].index(ide)
-        self.table_var[scope]['value'][index] = value
+    #método pra atribuir valor
+    def assign_var(self, scope, ide, value, assign_type):
+        if not self.__contains_var(scope, ide): #verifica se a variavel não existe
+            print('FAZER A CHAMADA DO ERRO AQUI: #variável não declarada anteriormente') 
+        else:
+            if(assign_type == 'primitivo'): #se for uma atribuição com valores normais
+                if self.__is_corect_type(scope, ide, value): #verifica se o tipo ta certo
+                    index = self.table_var[scope]['ide'].index(ide)
+                    self.table_var[scope]['value'][index] = value #bota na tabela
+                else:
+                    print('FAZER A CHAMADA DO ERRO AQUI: #tipo incompatível')
+        
+            elif(assign_type == 'variavel'): 
+                self.__assign_var_to_var(scope, ide, value)
 
+            elif(assign_type == 'func'): 
+                self.__assign_func_to_var(scope, ide, value)
+
+            elif(assign_type == 'exp'):
+                pass
+
+    def __assign_var_to_var(self, scope, ide, value):
+        if not self.__contains_var(scope, value): #verifica se a variavel não existe
+            print('FAZER A CHAMADA DO ERRO AQUI: #variável não declarada anteriormente') 
+        else:
+            index_var1 = self.table_var[scope]['ide'].index(ide)
+            tipo_var1 = self.table_var[scope]['tipo'][index_var1]
+
+            index_var2 = self.table_var[scope]['ide'].index(value)
+            tipo_var2 = self.table_var[scope]['tipo'][index_var2]
+            value_var2 = self.table_var[scope]['value'][index_var2]
+            
+            if(tipo_var1 == tipo_var2):
+                self.table_var[scope]['value'][index_var1] = value_var2 #bota na tabela
+            else:
+                print('FAZER A CHAMADA DO ERRO AQUI: #tipo incompatível')
+
+    def __assign_func_to_var(self, scope, ide, value):
+        if not self.__contains_func(value): #verifica se a função não existe
+            print('FAZER A CHAMADA DO ERRO AQUI: #função não declarada') 
+        else:
+            index_var = self.table_var[scope]['ide'].index(ide)
+            tipo_var = self.table_var[scope]['tipo'][index_var]
+
+            index_func = self.table_func['ide'].index(value)
+            tipo_func = self.table_func['tipo'][index_func]
+            
+            if(tipo_var == tipo_func):
+                self.table_var[scope]['value'][index_var] = value #verificar oq vai ficar armazenado
+            else:
+                print('FAZER A CHAMADA DO ERRO AQUI: #tipo incompatível')
+
+
+    def __is_corect_type(self, scope, ide, value):
+        index = self.table_var[scope]['ide'].index(ide)
+        tipo = self.table_var[scope]['tipo'][index]
+
+        if tipo == 'int':
+            if isinstance(int(value), int):
+                return True
+            else:
+                return False
+
+        if tipo == 'float':
+            if re.match(r'^-?\d+(?:\.\d+)$', value) is None:
+                return False
+            else:
+                return True
+
+        if tipo == 'string':
+            if (value != 'false' and value != 'true') and not value[0].isdigit():
+                return True
+            else:
+                return False
+
+        if tipo == 'boolean':
+            if value == 'false' or value == 'true':
+                return True
+            else:
+                return False
+    
 
     #MÉTODOS PARA MANIPULAR A TABELA DE STRUCTS
     def contains_struct(self, ide, scope):
@@ -73,7 +154,7 @@ class semantic_analyzer:
 
 
     #MÉTODOS PARA MANIPULAR A TABELA DE FUNÇÕES
-    def contains_func(self, ide):
+    def __contains_func(self, ide):
         return True if ide in self.table_func['ide'] else False
 
     def add_func(self, ide, tipo, type_params, params):
